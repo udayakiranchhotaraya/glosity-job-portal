db27cb2e39b2ae70689f769ba26e78c9d234025f
udayakiranchhotaraya <udayakiranchhotaraya@gmail.com>
2024-06-09 01:16:03 +0530

    App configuration done.

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,3 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
+const app = express();
 
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(cookieParser());
+app.use(express.static('/public'));
+
+module.exports = app;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,19 @@
-const express = require('express');
-
 require('dotenv').config();
 
 const dbConnect = require('./db/db.config');
 
+const app = require('./app');
+
+const PORT = process.env.PORT || 5000;
+const SERVER_URL = `http://localhost:${PORT}`;
+
 (async function () {
     try {
-        await dbConnect()
+        await dbConnect();
+        app.on('error', (error) => {
+            console.error(`Express app initialisation error: ${error}`);
+        })
+        app.listen(PORT, () => console.log(`Server started at ${SERVER_URL}`));
     } catch (error) {
         console.error(error);
         process.exit(1);
